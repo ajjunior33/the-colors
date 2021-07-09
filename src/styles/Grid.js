4a9892a5a9d30676561550735c572d4262561c59
Andre Souza <andre.oliveira@salesfarm.com.br>
2021-07-09 00:16:22 -0300

    Configurações .

--- src/styles/Grid.js
+++ src/styles/Grid.js
@@ -3,10 +3,13 @@ import styled from "styled-components";
 export const Row = styled.div`
   display: flex;
   flex-direction: row;
+  @media (max-width: 640px) {
+    flex-direction: column;
+  }
 `;
 
 export const Col = styled.div`
-  position:relative;
+  position: relative;
   width: 100%;
   max-width: 200px;
   height: 70vh;
@@ -17,26 +20,17 @@ export const Col = styled.div`
   justify-content: center;
   align-items: center;
 
-  background-color:  ${(props) => props.variant || "#16a085"};
+  background-color: ${(props) => props.variant || "#16a085"};
   color: ${(props) => props.color || "#FFF"};
 
   border-radius: 8px;
   margin: 30px 10px;
   font-family: "Nunito Sans", sans-serif;
   font-weight: bold;
-  @media (max-width: 0px) {
-    max-width: 180px;
-  }
-  @media (max-width: 576px) {
-    max-width: 220px;
-  }
-  @media (max-width: 768px) {
-    max-width: 260px;
-  }
-  @media (max-width: 992px) {
-    max-width: 300px;
-  }
-  @media (max-width: 1200px) {
-    max-width: 340px;
+
+  @media (max-width: 640px) {
+    max-width: 100%;
+    height: 150px;
+    margin: 10px 0;
   }
 `;
