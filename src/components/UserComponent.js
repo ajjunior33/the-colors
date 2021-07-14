ab4ca207abdb5effab94281c10b3436a187b062c
André Souza <ajjunior33@gmail.com>
2021-07-14 15:05:59 -0300

    Start code login.

--- src/components/UserComponent.js
+++ src/components/UserComponent.js
@@ -1,10 +1,46 @@
-import { PhotoUser } from '../styles/User';
-import defaultPhoto from '../assets/images/user.png';
+import { useState } from "react";
+import { GrGoogle } from "react-icons/gr";
+
+//Images
+import defaultPhoto from "../assets/images/user.png";
+
+//Styles
+import { PhotoUser } from "../styles/User";
+import { DropdownList, DropdownPanel } from "../styles/Dropdown";
+import { ButtonBlock } from "../styles/Button";
+
 const UserComponent = () => {
+  const [active, setActive] = useState(false);
+
+  function handleActiveDropdown() {
+    if (active === true) {
+      setActive(false);
+    } else {
+      setActive(true);
+    }
+  }
+
   return (
-    <PhotoUser>
-      <img src={defaultPhoto} alt="avatar" />
-    </PhotoUser>
+    <>
+      <DropdownPanel onClick={handleActiveDropdown}>
+        <PhotoUser onClick={handleActiveDropdown}>
+          <img src={defaultPhoto} alt="avatar" />
+        </PhotoUser>
+
+        {active === true && (
+          <DropdownList>
+            <ButtonBlock
+              inputColorVariant="#3742fa"
+              inputColor="#5352ed"
+              color="#ced6e0"
+            >
+              <GrGoogle />
+              Login / Criar Conta
+            </ButtonBlock>
+          </DropdownList>
+        )}
+      </DropdownPanel>
+    </>
   );
 };
 
--- src/styles/Button.js
+++ src/styles/Button.js
@@ -4,7 +4,7 @@ export const Button = styled.button`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content:center;
+  justify-content: center;
 
   padding: 8px 15px;
   border: 0;
@@ -21,8 +21,40 @@ export const Button = styled.button`
 
   cursor: pointer;
 
-  svg{
-    margin-right:5px;
+  svg {
+    margin-right: 5px;
+  }
+  &:hover {
+    filter: brightness(90%);
+    box-shadow: inset 2px 0px 10px
+      ${(props) => props.inputColorVariant || "#bdc3c7"};
+  }
+`;
+
+export const ButtonBlock = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  width:100%;
+  padding: 8px 15px;
+  border: 0;
+  border-radius: 5px;
+
+  background-color: ${(props) => props.inputColor || "#ecf0f1"};
+  border: 1px solid ${(props) => props.inputColorVariant || "#bdc3c7"};
+  box-shadow: inset 2px 0px 5px
+    ${(props) => props.inputColorVariant || "#bdc3c7"};
+
+  transition: 500ms all;
+
+  color: ${(props) => props.color || "#2c3e50"};
+
+  cursor: pointer;
+
+  svg {
+    margin-right: 5px;
   }
   &:hover {
     filter: brightness(90%);
